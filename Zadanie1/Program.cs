2b8da9dc029d4d711ee947fa827900299e6983de
DiasKapassov <diasgoose@gmail.com>
2023-04-13 21:28:57 +0600

    Сделал 1 задание

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Числа через пробел: ");
+string[] array = Console.ReadLine().Split(' ');
+
+int Count(string[] array)
+{
+    int c = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (int.Parse(array[i]) > 0) c++;
+    }
+
+    return c;
+}
+Count(array);
+
+Console.WriteLine(String.Join(" / ", array));
+Console.WriteLine(Count(array));    
